f28b78868e8c04657bd944979137bf6ec26e70f4
ikostan <igorkostan@gmail.com>
2018-01-18 20:40:37 -0800

    Add project files.

---
+++ Csharp_Collections_Fundamentals/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Csharp_Collections_Fundamentals
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
