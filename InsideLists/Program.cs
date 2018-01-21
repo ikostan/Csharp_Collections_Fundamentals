f690d7b78a7cdaf8cba6d862d7a074cd360c298b
ikostan <igorkostan@gmail.com>
2018-01-20 21:18:19 -0800

    InsideLists

---
+++ InsideLists/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace InsideLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inside Lists: index based collection\n");
+
+
+
+            Console.WriteLine("Please enter any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
