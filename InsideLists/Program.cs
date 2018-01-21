a8fa98aefa227e280c6b02a2330afe97fdb1ba6f
ikostan <igorkostan@gmail.com>
2018-01-20 21:38:06 -0800

    InsideLists

--- InsideLists/Program.cs
+++ InsideLists/Program.cs
@@ -9,16 +9,40 @@ namespace InsideLists
         {
             Console.WriteLine("Inside Lists: index based collection\n");
 
-            //Basic declaration + methods
-            List<string> names = new List<string>() { "John", "Donald", "Bob", "Sean", "Frank" };
-            Console.WriteLine($"Size: {names.Count}");
-            names.Add("Bruce");
-            Console.WriteLine($"Size: {names.Count}");
-            Console.WriteLine($"Last added name: {names[names.Count - 1]}");
-
+            BasicOperations();
 
             Console.WriteLine("Please enter any key to exit...");
             Console.ReadKey();
         }
+
+        private static void BasicOperations()
+        {
+            //Basic declaration + methods
+            List<string> names = new List<string>(5) { "John", "Donald", "Bob", "Sean", "Frank" };
+
+            Console.WriteLine($"Size: {names.Count}");
+            Console.WriteLine($"Capacity: {names.Capacity}");
+            names.Add("Bruce");
+
+            Console.WriteLine($"Size: {names.Count}");
+            Console.WriteLine($"Capacity: {names.Capacity}");
+            Console.WriteLine($"Last added name: {names[names.Count - 1]}");
+
+            names.Remove("John");
+            Console.WriteLine($"First name in the list: {names[0]}");
+            Console.WriteLine($"Size: {names.Count}");
+            Console.WriteLine($"Capacity: {names.Capacity}");
+
+            names.RemoveAt(names.Count - 1);
+            Console.WriteLine($"Last name in the list: {names[names.Count - 1]}");
+            Console.WriteLine($"Size: {names.Count}");
+            Console.WriteLine($"Capacity: {names.Capacity}");
+
+            names.Clear();
+            Console.WriteLine($"Size: {names.Count}");
+            Console.WriteLine($"Capacity: {names.Capacity}");
+        }
+
+        //End of Class
     }
 }
