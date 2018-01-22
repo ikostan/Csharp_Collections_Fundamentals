1043dc606c5e7443a0d4e95f398cdc95fa6063cd
ikostan <igorkostan@gmail.com>
2018-01-21 19:18:44 -0800

    InsideSets

---
+++ InsideSets/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace InsideSets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inside Sets:\n");
+
+            Console.WriteLine("Please enter any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
