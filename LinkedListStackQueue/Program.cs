0d31e235629741c86425b6f745c1f1d3481adc74
ikostan <igorkostan@gmail.com>
2018-01-21 18:31:31 -0800

    LinkedListStackQueue

---
+++ LinkedListStackQueue/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace LinkedListStackQueue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Linked Lists, Stacks and Queues:\n");
+
+            SampleLinkedList();
+            SampleLinkedListNode();
+            SampleStack();
+            SampleQueue();
+
+            Console.WriteLine("Please press any key to exit...\n");
+            Console.ReadKey();
+        }
+
+        private static void SampleLinkedList()
+        {
+            Console.WriteLine("Linked Lists - allows fast adding/removing elements\n");
+
+        }
+
+        private static void SampleLinkedListNode()
+        {
+            Console.WriteLine("Linked List Node - required to store items in a linked list\n");
+
+        }
+
+        private static void SampleStack()
+        {
+            Console.WriteLine("Stack - LIFO\n");
+
+        }
+
+        private static void SampleQueue()
+        {
+            Console.WriteLine("Queue - FIFO\n");
+
+        }
+
+        //End of class
+    }
+}
