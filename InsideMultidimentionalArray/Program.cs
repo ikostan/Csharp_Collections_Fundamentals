b35df93ee2489098e6e2dde71e8ce57268d7bc3e
ikostan <igorkostan@gmail.com>
2018-01-24 15:18:30 -0800

    Inside Multidimentional Arrays

---
+++ InsideMultidimentionalArray/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InsideMultidimentionalArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Inside Multidimentional Arrays:\n");
+
+
+
+            Console.WriteLine("Please press any key to exit...\n");
+            Console.ReadKey();
+        }
+    }
+}
