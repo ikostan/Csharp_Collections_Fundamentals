5b49db1aa9d1767a8de272db4a8cc9540fd019dd
ikostan <igorkostan@gmail.com>
2018-01-24 16:47:32 -0800

    //Using bounds

--- InsideMultidimentionalArray/Program.cs
+++ InsideMultidimentionalArray/Program.cs
@@ -29,10 +29,24 @@ namespace InsideMultidimentionalArray
             };
 
             Console.WriteLine($"Total length: {twoDarray.Length}\n");
+            Console.WriteLine($"Rank (# of dimentions): {twoDarray.Rank}\n");
 
+            //Using length
             for (int row = 0; row < twoDarray.GetLength(0); row++)
             {
                 for (int col = 0; col < twoDarray.GetLength(1); col++)
+                {
+                    Console.Write(twoDarray[row, col] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+
+            //Using bounds
+            for (int row = twoDarray.GetLowerBound(0); row <= twoDarray.GetUpperBound(0); row++)
+            {
+                for (int col = twoDarray.GetLowerBound(1); col <= twoDarray.GetUpperBound(1); col++)
                 {
                     Console.Write(twoDarray[row, col] + ", ");
                 }
