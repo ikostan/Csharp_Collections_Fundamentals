c557f9367cff67f1df4c99811fdfda099f84ff16
ikostan <igorkostan@gmail.com>
2018-01-24 16:55:16 -0800

    sampleJaggedArray

--- InsideMultidimentionalArray/Program.cs
+++ InsideMultidimentionalArray/Program.cs
@@ -12,8 +12,8 @@ namespace InsideMultidimentionalArray
         {
             Console.WriteLine("Inside Multidimentional Arrays:\n");
 
-            sampleMultiDimentionalArray();
-
+            //sampleMultiDimentionalArray();
+            sampleJaggedArray();
 
             Console.WriteLine("Please press any key to exit...\n");
             Console.ReadKey();
@@ -49,6 +49,33 @@ namespace InsideMultidimentionalArray
                 for (int col = twoDarray.GetLowerBound(1); col <= twoDarray.GetUpperBound(1); col++)
                 {
                     Console.Write(twoDarray[row, col] + ", ");
+                }
+                Console.WriteLine();
+            }
+
+            Console.WriteLine();
+        }
+
+        private static void sampleJaggedArray()
+        {
+            int[][] jArray = new int[3][]
+            {
+                new int[]{ 1, 2, 3, 4, 5 },
+                new int[]{ 6, 7, 8, 9, 10, 11 } ,
+                new int[]{ 12, 13, 14, 15, 15, 16, 17 }
+            };
+
+            Console.WriteLine($"Total length: {jArray.Length}\n");
+            Console.WriteLine($"Rank (# of dimentions): {jArray.Rank}\n");
+
+            //Using length
+            for (int row = 0; row < jArray.Length; row++)
+            {
+                Console.WriteLine($"\nTotal length: {jArray[row].Length}\n");
+                Console.WriteLine($"Rank (# of dimentions): {jArray[row].Rank}\n");
+                for (int col = 0; col < jArray[row].Length; col++)
+                {
+                    Console.Write(jArray[row][col] + ", ");
                 }
                 Console.WriteLine();
             }
